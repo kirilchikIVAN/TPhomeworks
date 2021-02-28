c7ba31bec95706c4d5d4dd55d958ad0286aa3bbb
ivan-kirilchik <kirill_ivanchik@mail.ru>
2021-02-28 18:24:42 +0300

    done in run added

--- task1/run.sh
+++ task1/run.sh
@@ -3,7 +3,6 @@ echo $str
 mkdir $3
 for file in `find . -type f -name $str`
 do
-   echo $(basename "$file" | cut -d. -f1)
    newname=$(basename "$file")
    while [ -e $3/$newname ]
    do
@@ -13,3 +12,4 @@ do
    cp $file $3/$newname
 done
 tar -cf $(basename "$4" | cut -d. -f1).tar.gz $3
+echo 'done'
