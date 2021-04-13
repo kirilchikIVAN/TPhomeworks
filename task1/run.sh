43dc64a6847b80ecd0f55187b7ee9b7e4eb9550e
ivan-kirilchik <kirill_ivanchik@mail.ru>
2021-04-13 19:23:08 +0300

    task1

---
+++ task1/run.sh
@@ -0,0 +1,15 @@
+str='*.'$2
+mkdir $3
+for file in `find $1 -type f -name $str`
+    do
+        newname=$(basename "$file")
+        while [ -e $3/$newname ]
+        do
+            newname=$(basename "$newname" | cut -d. -f1)1.$2
+        done
+ 
+        cp $file $3/$newname
+done
+tar czf $4 $3
+echo 'done'
+
